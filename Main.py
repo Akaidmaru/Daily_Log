20ed40b4351545790c34c0a4c94fb1c6a252b7cf
Reddmar Quevedo <reddmar.quevedo@gmail.com>
2021-05-07 20:24:15 -0500

    Fran's mods on code. He added Excel Support, factorized the apped of data in the workbook, also there is use of the panda.

--- Main.py
+++ Main.py
@@ -1,9 +1,22 @@
 # EXPORT DATETIME SO I CAN GET THE EXACT TIME OF THE CALL
 # CREATE DICTIONARY TO WORK
 from datetime import datetime as dt
-filename = 'daily_report.txt'
+import pandas as pd 
+import openpyxl
+import time
+dt = pd.Timestamp('2021-09-01 12:44:12')
+dt.round('min')
+wb = openpyxl.Workbook()
+hoja = wb.active
+hoja.append(('Start Time', 'End Time', 'Phone Number', 'Status', 'Policy', 'Note'))
+
 policy = '-'
-report = {}
+report = {()}
+
+def agregar_valores():  # FUNCTION TO ADD THE LAST VALUES OF THE CLAIM AND SAVE.
+    for r in result:
+        hoja.append(r)
+    wb.save('prueba3.xlsx')
 # I START A INFINITE LOOP TO KEEP THE PROGRAM WORKING
 while True:
     # I ASK FOR Q TO EXIT OR BLANK TO CONTINUE.
@@ -11,25 +24,26 @@ while True:
     if flag == 'q':
         break
     else:
-        start_time = dt.now()
+        start_time = time.strftime("%I:%M") #dt.now()
         p_n = input("Phone number: ")
         note = input("Reason:").lower()
 #INFO, 2) CLAIM, 3) INTERPRETATION, 4) PAYMENT , 5) UPDATES, 6) UW
         if note == 'claim' or note == 'update' or note == 'payment':
             policy = input("Policy Number: ")
             status = input("Insert status to get end time:")
-            end_time = dt.now()
-            result = str(start_time) + ' ' + str(end_time) + ' ' + \
-                p_n + ' ' + status + ' ' + policy + ' ' + note  
-            with open(filename, 'a') as file_object:
-                file_object.write(result)
+            end_time =  time.strftime("%I:%M") # MODIFIED TIME FOR ONLY HOUR AND MIN.
+            result = {(start_time, end_time,
+                p_n, status, policy, note)}
+            agregar_valores()
         elif note == 'info' or note == 'interpretation' or note == 'uw':
             status = input("Insert status to get end time:")
-            end_time = dt.now()
-            result = str(start_time) + ' ' + str(end_time) + ' ' + \
-                p_n + ' ' + status + ' ' + policy + ' ' + note
-            with open(filename, 'a') as file_object:
-                file_object.write(result)
+            policy = '-'
+            end_time =  time.strftime("%I:%M") # MODIFIED TIME FOR ONLY HOUR AND MIN.
+            result = {(start_time, end_time,
+                p_n, status, policy, note)}
+            agregar_valores()
+            
+        
 
 # HERE THE REPORT SHOULD BE SAVED.
 
